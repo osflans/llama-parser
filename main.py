21ca67ad98a3f07e47b57d66ed2ec36703614cdf
osflans <147162338+osflans@users.noreply.github.com>
2024-10-04 18:53:37 +0300

    add files

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from parse import parse_with_ollama
+
+st.title("AI Web Scraper")
+url = st.text_input("Enter a Website URL: ")
+
+if st.button("Scrape Site"):
+
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse")
+
+    if st.button("Parse Content"):
+        if parse_description:
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
+
+
+
